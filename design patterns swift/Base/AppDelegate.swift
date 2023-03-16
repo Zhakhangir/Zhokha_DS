9a63a0fa66c081494dde7694b32a37d88ff566ee
Жахангир Таиров <jahangirtairov123@gmail.com>
2023-03-16 12:24:27 +0600

    recived push notification

--- design patterns swift/Base/AppDelegate.swift
+++ design patterns swift/Base/AppDelegate.swift
@@ -8,22 +8,27 @@
 import UIKit
 import UserNotifications
 import FirebaseCore
+import FirebaseMessaging
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-            
-        let pushManager = PushNotificationManager(userID: "tam_tam_tam")
-        pushManager.registerForPushNotifications()
-        FirebaseApp.configure()
         
         window = UIWindow()
         window?.rootViewController = ViewController()
         window?.makeKeyAndVisible()
         
+        FirebaseApp.configure()
+        FirebaseConfiguration.shared.setLoggerLevel(.min)
+        let pushManager = PushNotificationManager(userID: "tam_tam_tam")
+        pushManager.registerForPushNotifications()
         
         return true
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
     }
 }
